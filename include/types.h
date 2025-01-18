3c3fd8468f2993c07ad8b5288c218b109eeec164
Andrea Cardone <cardone.andrea97@gmail.com>
2025-01-19 00:11:59 +0100

    added new types

--- include/types.h
+++ include/types.h
@@ -2,6 +2,69 @@
 #define __TYPES_H
 
 #include <stdint.h>
+#include <iostream>
+#include <stdexcept>
+
+// Registers
+#define AD5933_CTRL_REG_MSB        0x80
+#define AD5933_CTRL_REG_LSB        0x81
+#define AD5933_START_FREQ1         0x82
+#define AD5933_START_FREQ2         0x83
+#define AD5933_START_FREQ3         0x84
+#define AD5933_FREQ_INC1           0x85
+#define AD5933_FREQ_INC2           0x86
+#define AD5933_FREQ_INC3           0x87
+#define AD5933_NUMB_INCR_MSB       0x88
+#define AD5933_NUMB_INCR_LSB       0x89
+#define AD5933_NUMB_SETT_MSB       0x8A
+#define AD5933_NUMB_SETT_LSB       0x8B
+#define AD5933_STATUS_REG          0x8f
+#define AD5933_TEMP_DATA_MSB       0x92
+#define AD5933_TEMP_DATA_LSB       0x93
+#define AD5933_REAL_DATA_MSB       0x94
+#define AD5933_REAL_DATA_LSB       0x95
+#define AD5933_IMAG_DATA_MSB       0x96
+#define AD5933_IMAG_DATA_LSB       0x97
+
+// Control register 0x80
+#define MASK_CTRL_FUNCTION         0xF0
+#define MASK_CTRL_NO_OPERATION     0x00
+#define MASK_CTRL_INIT_START_FREQ  0x10
+#define MASK_CTRL_START_FREQ_SWEEP 0x20
+#define MASK_CTRL_INCR_FREQ        0x30
+#define MASK_CTRL_REPEAT_FREQ      0x40
+#define MASK_CTRL_NO_OPERATION_1   0x80
+#define MASK_CTRL_MEASURE_TEMP     0x90
+#define MASK_CTRL_PWR_DOWN_MODE    0xA0
+#define MASK_CTRL_STANDBY_MODE     0xB0
+#define MASK_CTRL_NO_OPERATION_2   0xC0
+#define MASK_CTRL_NO_OPERATION_3   0xD0
+
+#define MASK_CTRL_OUT_V_RANGE      0x0C
+#define MASK_CTRL_2_V_PP           0x00
+#define MASK_CTRL_200_MV_PP        0x02
+#define MASK_CTRL_400_MV_PP        0x04
+#define MASK_CTRL_1_V_PP           0x06
+
+#define MASK_CTRL_PGA_FIELD        0x01
+#define MASK_CTRL_PGA_X5           0x00
+#define MASK_CTRL_PGA_X1           0x01
+
+// Control register 0x81
+#define MASK_CTRL_RESET            0x10
+#define MASK_CTRL_SYS_CLK_FIELD    0x08
+#define MASK_CTRL_EXT_SYS_CLK      0x08
+#define MASK_CTRL_INT_SYS_CLK      0x00
+
+// Status register 0x8F
+#define MASK_STS_TEMP_VALID        0x01
+#define MASK_STS_IMPED_VALID       0x02
+#define MASK_STS_FRQ_SWP_DONE      0x04
+
+#define MAX_INCREMENT_NUMBER  511
+#define MAX_SETTL_TIME_CYCLES 511
+
+char const* AD5933_FW_PATH = "../cypressfw/AD5933_34FW.hex";
 
 /**
  * \brief Structure to hold raw temperature data.
@@ -10,7 +73,7 @@ typedef struct
 {
   uint8_t temp_msb; /**< Most significant byte of the temperature data. */
   uint8_t temp_lsb; /**< Least significant byte of the temperature data. */
-} stTemperatureRaw_t;
+} TemperatureRaw_st;
 
 /**
  * \brief Type definition for processed temperature data.
@@ -26,7 +89,7 @@ typedef struct
   uint8_t real_lsb;
   uint8_t imag_msb;
   uint8_t imag_lsb;
-} stImpedDataRaw_t;
+} ImpedDataRaw_st;
 
 enum ClockConfiguration_t
 {
@@ -70,11 +133,31 @@ enum CalibrationMode_t
   MULTI_POINT  
 };
 
+enum Ad5933Function_t
+{
+  NO_OPERATION,
+  INIT_WITH_START_FREQ,
+  START_FREQ_SWEEP,
+  INCREMENT_FREQUENCY,
+  REPEAT_FREQUENCY,
+  NO_OPERATION_1,
+  MEASURE_TEMPERATURE,
+  POWER_DOWN_MODE,
+  STANDBY_MODE,
+  NO_OPERATION_2,
+  NO_OPERATION_3
+};
+
 typedef double ResistorValue_t;
 typedef double CapacitorValue_t;
 typedef double Frequency_t;
 typedef double GainFactor_t;
-typedef double InternalTemperature_t;
+typedef double Temperature_t;
+typedef uint8_t Status_t;
 
+class CyusbError : public std::runtime_error {
+public:
+  CyusbError(const std::string& message) : std::runtime_error(message) {}
+};
 
 #endif
